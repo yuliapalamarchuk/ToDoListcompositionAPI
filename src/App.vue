7c2765d615f26ef44e9d67a849426ec415ebd17e
Yulia <you@example.com>
2023-05-01 08:59:49 +0300

    2.2

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,79 @@
-<template>My To Do List</template>
+<template>
+  <div class="wrapper-todo">
+    <div class="title has-text-centered">My ToDo List!</div>
+    <form @submit.prevent="addTodo">
+      <div class="field is-grouped mb-5">
+        <p class="control is-expanded">
+          <input
+            v-model="newToDoContent"
+            class="input"
+            type="text"
+            placeholder="Add to do"
+          />
+        </p>
+        <p class="control">
+          <button :disabled="!newToDoContent" class="button is-info">
+            Add
+          </button>
+        </p>
+      </div>
+    </form>
+
+    <div v-for="todo in todos" :key="todo.id" class="card mb-5">
+      <div class="card-content">
+        <div class="content">
+          <div class="columns is-mobile is-vcentered">
+            <div class="column">{{ todo.content }}</div>
+            <div class="column is-5 has-text-right">
+              <button class="button is-light">&check;</button>
+              <button class="button is-danger ml-2">&cross;</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script setup>
+// import
+import { ref } from "vue";
+import { v4 as uuidv4 } from 'uuid';
+
+
+// todo Array
+const todos = ref([]);
+
+
+// input
+const newToDoContent = ref("");
+
+
+// send form
+const addTodo = () => {
+  const newTodo = {
+    id: uuidv4(),
+    content: newToDoContent.value,
+  };
+  todos.value.push(newTodo);
+  newToDoContent.value = "";
+};
+</script>
+
+
+
+
+
+
+
+
+
+<style>
+@import "bulma/css/bulma.min.css";
+.wrapper-todo {
+  padding: 20px;
+  margin: 0 auto;
+  max-width: 400px;
+}
+</style>
